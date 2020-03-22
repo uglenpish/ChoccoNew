32f7ea5d5fb0ecc3ae16285d8bf29432a110b102
Dmitry Sterlin <uglenpish@rambler.ru>
2020-03-22 18:28:47 +0300

    не работает js

---
+++ script.js
@@ -0,0 +1,16 @@
+let nav = document.querySelector('#overlay');
+// let body = document.querySelector('body');
+// let burger = document.querySelector('.hamburger');
+
+// function toggleMenu() {
+// 	nav.classList.toggle('.menu__burger--activ');
+// }
+
+// burger.addEventListener('click', toggleMenu());
+
+let nav = document.querySelector('#overlay');
+let burger = document.querySelector('.hamburger');
+
+burger.addEventListener('click', function(){
+	console.log('asd');
+})
