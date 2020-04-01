3fbc77d2137258ddeea352a7e89833bec19edf39
Dmitry Sterlin <uglenpish@rambler.ru>
2020-04-01 21:42:53 +0300

    пытаюсь сделать слайдер

--- slider.js
+++ slider.js
@@ -1,25 +1,69 @@
-let left = document.querySelector(".left");
-let right = document.querySelector(".right");
-let slider = document.querySelector(".slider__list");
-let widthContainer = document.querySelector('.slider').clientWidth;
-const item = document.querySelector(".slider__item");
-let controls = document.querySelector('.bar__scroll');
+
+let slider = document.querySelector(".card");
+let widthContainer = document.querySelector('.card__wrap').clientWidth;
+const item = document.querySelector(".card__item");
+let controls = document.querySelector('.photo');
+var pos = 0;
 
 function calcWidthList() {
-    const itemsCount = slider.children.length;
-    const widthList = itemsCount * widthContainer; 
+  const itemsCount = slider.children.length;
+  const widthList = itemsCount * widthContainer; 
 
-    slider.style.width = `${widthList}px`;
+  slider.style.width = `${widthList}px`;
 }
 
-// function handlerClick(event) {
-//     if (event.target.tagName === 'SVG') {
-//         slide(event.target);
-//         console.log('1');
-//     }
-// }
+function handlerClick(event) {
+	e.preventDefault();
+  if (event.target.tagName === 'A') {
+    slide(event.target);
+  }
+}
+
+function slide(target) {
+  const vector = target.dataset.vector;// prev or next
+
+  switch (vector) {
+    case 'next':
+     	slideTo(vector);
+      break;
+    case 'prev':
+      slideTo(vector);
+      break;
+    // case '3':
+    //   slideTo(vector);
+    //   break;
+    }
+}
+
+function slideTo(vector) {
+    const active = document.querySelector('.vision');
+
+    if (vector === 'next') {
+        var nextElement = active.nextElementSibling;
+    } else {
+        var prevElement = active.previousElementSibling;
+    }
+
+    if (nextElement) {
+        pos -= widthContainer;
+        active.classList.remove('vision');
+        nextElement.classList.add('vision');
+        translate(pos);
+    } else if (prevElement) {
+        pos += widthContainer;
+        active.classList.remove('vision');
+        prevElement.classList.add('vision');
+        translate(pos);
+    }
+}
+
+controls.addEventListener('click', handlerClick);
 
 window.addEventListener('load', calcWidthList);
+
+
+
+// слайдер батончики
 
 // right.addEventListener("click", function(e) {
 //   loop("right", e);
@@ -45,52 +89,17 @@ window.addEventListener('load', calcWidthList);
 
 
 
-// window.addEventListener('load', calcWidthList);
 
 
 
 
 
-// function handlerClick(event) {
-//     if (event.target.tagName === 'SVG') {
-//         slide(event.target);
-//     }
-// }
 
-// function slide(target) {
-//     const vector = target.dataset.vector;// prev or next
 
-//     switch (vector) {
-//         case 'next':
-//             slideTo(vector);
-//             break;
-//         case 'prev':
-//             slideTo(vector);
-//             break;
-//     }
-// }
 
-// function slideTo(vector) {
-//     const active = document.querySelector('.vision');
 
-//     if (vector === 'next') {
-//         var nextElement = active.nextElementSibling;
-//     } else {
-//         var prevElement = active.previousElementSibling;
-//     }
 
-//     if (nextElement) {
-//         pos -= widthContainer;
-//         active.classList.remove('vision');
-//         nextElement.classList.add('vision');
-//         translate(pos);
-//     } else if (prevElement) {
-//         pos += widthContainer;
-//         active.classList.remove('vision');
-//         prevElement.classList.add('vision');
-//         translate(pos);
-//     }
-// }
+
 
 // function translate(pos) {
 //   list.style.transform = `translateX(${pos}px)`;
